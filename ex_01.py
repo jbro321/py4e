dd226fa0f26fdee01dec5975efa8fefa3a185c7f
jbro321 <jaehyung0321@gmail.com>
2021-10-07 22:05:02 +0900

    update ex_01

--- ex_01.py
+++ ex_01.py
@@ -1,7 +1,5 @@
 '''
-Chapter1
-
-Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'.
+1. Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'.
 '''
 
 print("Hello!!")
